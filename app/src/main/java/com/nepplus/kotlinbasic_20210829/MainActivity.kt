64f30bdb1f5da768cbe01ec8588193eae1ac38fd
김다은 <softeun92@naver.com>
2021-08-29 10:22:05 +0900

    로그버튼눌림ㅇㅇ

--- app/src/main/java/com/nepplus/kotlinbasic_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20210829/MainActivity.kt
@@ -2,10 +2,22 @@ package com.nepplus.kotlinbasic_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogBtn.setOnClickListener  {
+
+
+            Log.d("메인화면", "로그 버튼 눌림")
+
+        }
+
+
+
     }
 }
